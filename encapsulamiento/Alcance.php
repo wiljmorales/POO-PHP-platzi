e0a09ee23ffe51143802460e9637055b1a176653
Wilfredo Morales <wiljmoralesa@gmail.com>
2022-09-23 21:43:01 -0400

    encapsulamiento

---
+++ encapsulamiento/Alcance.php
@@ -0,0 +1,17 @@
+<?php
+
+class User {
+  // public
+  // protected
+  // private
+
+  public const PAGINATE = 25;
+
+  public $username;
+  // protected $username;
+  // private $username;
+
+  public function getUsername(){
+    //
+  }
+}
